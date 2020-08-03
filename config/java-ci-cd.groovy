39862390c3eb2f378cf36d4d3402dafd94ca20e1
supratik110 <13682744+supratik110@users.noreply.github.com>
2020-08-03 21:21:20 +0530

    update docker pull

--- config/java-ci-cd.groovy
+++ config/java-ci-cd.groovy
@@ -1,6 +1,6 @@
 def build(){
 			try{
-				dir(deployProps.dockePull){
+				dir(deployProps.dockerPull){
 					sh commonProps.mavenClean
 					status = "SUCCESSFUL"
 					echo 'BUILD SUCCESS'
@@ -9,28 +9,7 @@ def build(){
 			catch (e) {
 					status = "FAILED"
 						} 
-			finally {
-					notifyBuild(status)
-					} 
-			}
-					
-
-def notifyBuild(String buildStatus)
-{
-  def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
-  def summary = "${subject} (${env.BUILD_URL})"
-  def details = """<p>STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-    <p>Check console output at "<a href="${env.BUILD_URL}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>
-    <p>Please give input to deploy"<a href="${env.JENKINS_URL}/job/${env.JOB_NAME}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>"""
-	
- emailext (
-      subject: subject,
-      body: details,
-      to: commonProps.recipients
-    )
- 
-}
-
+	}
 def deploy(){
 			try	{
 				sh deployProps.dockerContainerId
